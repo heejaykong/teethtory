e58c5ea8e10bb4a5d27a29de12a5a6d5566b7e1a
SukheeChoi <95064440+SukheeChoi@users.noreply.github.com>
2022-04-13 17:28:03 +0900

    미니 프로젝트 공유

---
+++ src/main/java/com/mycompany/webapp/controller/HomeController.java
@@ -0,0 +1,24 @@
+package com.mycompany.webapp.controller;
+
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+	@RequestMapping("/")
+	public String home(Locale locale, Model model) {
+		logger.info("실행");
+		return "home";
+	}
+	
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,8 @@
+<%@ page session="false" language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+
+<%@ include file="/WEB-INF/views/common/header.jsp" %>
+
+<p>Home</p>
+
+<%@ include file="/WEB-INF/views/common/footer.jsp" %>
