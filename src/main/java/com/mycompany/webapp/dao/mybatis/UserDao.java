ac2ab999c4c327100afefbeac767fcc1d2e1b550
SukheeChoi <95064440+SukheeChoi@users.noreply.github.com>
2022-04-26 22:10:22 +0900

    users.xml 수정(insert 메소드에서 칼럼명 나열.). UserDao.java 수정.(deleteByUserid메소드 파라미터에 userpassword 추가.).

--- src/main/java/com/mycompany/webapp/dao/mybatis/UserDao.java
+++ src/main/java/com/mycompany/webapp/dao/mybatis/UserDao.java
@@ -1,10 +1,12 @@
 package com.mycompany.webapp.dao.mybatis;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.mycompany.webapp.dto.Users;
 
 public interface UserDao {
 	public Users selectByUserid(String userid);
 	public int insert(Users user);
 	public int update(Users user);
-	public int deleteByUserid(String userid);
+	public int deleteByUserid(@Param("userid") String userid, @Param("userpassword") String userpassword);
 }
