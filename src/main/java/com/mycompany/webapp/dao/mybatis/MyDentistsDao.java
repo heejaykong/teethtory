bbd46f986c9760c8edd089e60bfb9540a0222668
SukheeChoi <95064440+SukheeChoi@users.noreply.github.com>
2022-04-26 20:12:03 +0900

    mydentiststs.xml 작성. MyDentistsDao.java 수정.(insert, delete 메서드 추가.) component_scan.xml 수정.(repository 경로 수정.)

--- src/main/java/com/mycompany/webapp/dao/mybatis/MyDentistsDao.java
+++ src/main/java/com/mycompany/webapp/dao/mybatis/MyDentistsDao.java
@@ -7,4 +7,10 @@ import com.mycompany.webapp.dto.Pager;
 
 public interface MyDentistsDao {
 	public List<Mydentists> selectByUserid(String userid);
+	
+	//사용자가 치과 검색 후, 내 치과로 등록.
+	public int insert(Mydentists mydentists);
+	//사용자가 내 치과 목록에서 특정 치과 삭제.
+	public int delete(Mydentists mydentists);
+	
 }
