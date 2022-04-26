606702aba889af0bb7763f2873f605b372ebb643
SukheeChoi <95064440+SukheeChoi@users.noreply.github.com>
2022-04-26 22:33:46 +0900

    comments.xml

--- src/main/java/com/mycompany/webapp/dao/mybatis/CommentsDao.java
+++ src/main/java/com/mycompany/webapp/dao/mybatis/CommentsDao.java
@@ -2,14 +2,15 @@ package com.mycompany.webapp.dao.mybatis;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.mycompany.webapp.dto.Comments;
 import com.mycompany.webapp.dto.Pager;
 
 public interface CommentsDao {
-	public List<Comments> selectByPage(Pager pager);
+	public List<Comments> selectByPage(@Param("boardno") int boardno, @Param("pager") Pager pager);
 	public int count();
-	public Comments selectByBno(int bno);
 	public int insert(Comments comment);
-	public int deleteByBno(int bno);
 	public int update(Comments comment);
+	public int deleteByBno(int bno);
 }
