5502396fd1809e02696fc1768ea9982cdcb9d870
heejaykong <heejay.dev@gmail.com>
2022-04-27 14:31:06 +0900

    feat: 내가 작성한 글보기, 내가 작성한댓글보기 로직 추가(매퍼s, DAOs, Services)

--- src/main/java/com/mycompany/webapp/dao/mybatis/CommentDao.java
+++ src/main/java/com/mycompany/webapp/dao/mybatis/CommentDao.java
@@ -3,14 +3,17 @@ package com.mycompany.webapp.dao.mybatis;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.mycompany.webapp.dto.Comment;
 import com.mycompany.webapp.dto.Pager;
 
 @Mapper
 public interface CommentDao {
-	public int countPerBoardno(int boardno);
+	public int countByBoardno(int boardno);
+	public int countByUserid(String userid);
 	public List<Comment> selectByPage(int boardno, Pager pager);
+	public List<Comment> selectByUserid(@Param("userid") String userid, @Param("pager") Pager pager);
 	public int insert(Comment comment);
 	public int update(Comment comment);
 	public int deleteByCommentno(int commentno);
