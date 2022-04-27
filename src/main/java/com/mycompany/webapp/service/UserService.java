049442b6f6dc05c742600b7bf4373070e0ab2528
lee0507 <2eastbow@gmail.com>
2022-04-27 14:59:07 +0900

    UserService에 Login Join Method 추가

--- src/main/java/com/mycompany/webapp/service/UserService.java
+++ src/main/java/com/mycompany/webapp/service/UserService.java
@@ -2,6 +2,8 @@ package com.mycompany.webapp.service;
 
 import javax.annotation.Resource;
 
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.mycompany.webapp.dao.mybatis.UserDao;
@@ -12,6 +14,15 @@ import lombok.extern.log4j.Log4j2;
 @Service
 @Log4j2
 public class UserService {
+	
+	public enum JoinResult {
+		SUCCESS, FAIL, DUPLICATED
+	}
+	
+	public enum LoginResult {
+		SUCCESS, FAIL_MID, FAIL_MPASSWORD
+	}
+	
 	@Resource
 	private UserDao userDao;
 	
@@ -36,4 +47,35 @@ public class UserService {
 		log.info("삭제된 user의 아이디: " + userid);
 		return deletedRows;
 	}
+
+	public JoinResult join(User user) {
+		
+		User dbUser = userDao.selectByUserid(user.getUserid());
+		if(dbUser == null) {
+			PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//			PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+			user.setUserpassword(passwordEncoder.encode(user.getUserpassword()));
+			int result = userDao.insert(user);
+			return JoinResult.SUCCESS;		
+		} else {
+			return JoinResult.DUPLICATED;
+		}
+		
+	}
+
+	public LoginResult login(User user) {
+		User dbUser = userDao.selectByUserid(user.getUserid());
+		if(dbUser == null) {
+			return LoginResult.FAIL_MID;
+		} else {
+			PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+			if(passwordEncoder.matches(user.getUserpassword(), dbUser.getUserpassword())) {
+				return LoginResult.SUCCESS;
+			} else {
+				return LoginResult.FAIL_MPASSWORD;
+			}
+		}
+		
+	}
+
 }
