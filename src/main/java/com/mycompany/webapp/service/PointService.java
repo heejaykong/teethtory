be56f14cb84734c1cec32b0c4d3544250793b30a
SukheeChoi <95064440+SukheeChoi@users.noreply.github.com>
2022-04-27 11:00:22 +0900

    PointService.java 생성.

---
+++ src/main/java/com/mycompany/webapp/service/PointService.java
@@ -0,0 +1,73 @@
+package com.mycompany.webapp.service;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.mycompany.webapp.dao.mybatis.PointDao;
+import com.mycompany.webapp.dao.mybatis.UserDao;
+import com.mycompany.webapp.dto.Point;
+import com.mycompany.webapp.dto.User;
+
+import lombok.extern.log4j.Log4j2;
+
+@Service
+@Log4j2
+public class PointService {
+	@Resource
+	private PointDao pointDao;
+	
+	@Resource
+	private UserDao userDao;
+	
+	public boolean checkGotLoginPoint(String userid) {
+		if(pointDao.selectTodaysLoginPoint(userid) != null) {//로그인 포인트 획득 이력 행 존재.
+			return true;
+		} else {//로그인 획득 이력 행 부존재.
+			return false;
+		}
+	}
+	
+	//로그인 포인트 획득.
+	public boolean rewardLoginPoint(String userid) {
+		//당일에 로그인 포인트 획득 이력이 있는지 점검.
+		if(checkGotLoginPoint(userid) == false) {//로그인 포인트 획득X. 로그인 포인트 제공하면 됨.
+			Point point = new Point();
+			point.setUserid(userid);
+			point.setPointisplus(true);
+			point.setPointamount(100);
+			point.setPointdesc("로그인 포인트");
+			//pointdate는 쿼리문에서 sysdate로 입력.
+			pointDao.insert(point);
+			return true;
+		}
+		return false;
+	}
+	
+	//스케일링 할인.
+	public boolean usePointForScaling(String userid) {
+		//예약화면 표시할 때, 이미 1만 포인트 이상인지 점검했으므로,
+		//컨트롤러에서 메소드 호출되면, 무조건 1만포인트 차감+userpoint 업데이트.
+		
+		//users테이블의 userpoint 10,000포인트 차감해서 업데이트.
+		User user = new User();
+		user = userDao.selectByUserid(userid);
+		int userPoint = user.getUserpoint();
+		user.setUserpoint(userPoint-10000);
+		int updateResult = userDao.update(user);
+		
+		//points 테이블에 스케일링 할인이력 insert.
+		Point point = new Point();
+		point.setUserid(userid);
+		point.setPointisplus(false);
+		point.setPointamount(10000);
+		point.setPointdesc("스케일링 할인");
+		int insertResult = pointDao.insert(point);
+		
+		if(updateResult == 1 && insertResult == 1) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
