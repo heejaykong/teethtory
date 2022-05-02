e48293c54ed9f5c8920f78ccd8da2f94253cb6a5
lee0507 <2eastbow@gmail.com>
2022-05-02 21:22:16 +0900

    Treatment jsp에 비동기 작업

--- src/main/java/com/mycompany/webapp/controller/TreatmentController.java
+++ src/main/java/com/mycompany/webapp/controller/TreatmentController.java
@@ -1,9 +1,20 @@
 package com.mycompany.webapp.controller;
 
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mycompany.webapp.dto.Dentist;
+import com.mycompany.webapp.dto.User;
+import com.mycompany.webapp.service.MyDentistService;
+import com.mycompany.webapp.service.UserService;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -11,9 +22,27 @@ import lombok.extern.log4j.Log4j2;
 @RequestMapping("/treatment")
 @Log4j2
 public class TreatmentController {
+	
+	@Resource
+	private UserService userservice;
+	
+	@Resource
+	private MyDentistService myDentistService;
+	
 	@GetMapping("/main")
-	public String showTreatmentsList() {
+	public String showTreatmentsList(HttpSession session, Model model) {
 //		TBD: patientSsn에 해당하는 치료내역 목록 가져오기
+		String userid = (String) session.getAttribute("sessionUserid");
+		User user = userservice.getUser(userid);
+		String patientssn = user.getUserssn();
+		List<Dentist> dentist = myDentistService.getMyDentist(userid);
+		int dentistnum = myDentistService.getMyDentist(userid).size();
+		model.addAttribute("dentistnum", dentistnum);
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("dentist", dentist);
+		String json = jsonObject.toString();
+		model.addAttribute("dentist", json);
+		model.addAttribute("patientssn", patientssn);
 		return "treatment/main";
 //		또는
 //		return "treatment/emptyTreatmentList";
